a4a6c9eda718f5a04e0c2a9dcdd098aad5cfd074
Jeremy J Berg <jeremy.jackson.berg@gmail.com>
2014-04-16 13:48:54 -0700

    More work on LDCalc script. Very tedious.

--- Scripts/LDCalc.R
+++ Scripts/LDCalc.R
@@ -1,6 +1,38 @@
 library ( expm )
 library ( combinat )
-library(plyr)
+library ( plyr )
+library ( RColorBrewer )
+
+f <- 0.10
+N <- 100000
+r_x <- 10^-4
+r_y <- 10^-4
+s <- 0.01
+#### selected portion for ld across site
+SelTransMat <- function ( r_x , r_y , s = f , N ) {
+	p_x <- 1 - exp ( - r_x / s * log ( ( N - 1 ) / f - ( N - 1 ) ) )
+	q_x <- 1 - p_x
+	p_y <- 1 - exp ( - r_y / s * log ( ( N - 1 ) / f - ( N - 1 ) ) )
+	q_y <- 1 - p_y
+	
+	trans.mat <- matrix ( 	c ( 
+								q_x^2*q_y^2 , 0 , 2*p_x*q_x*q_y^2 , 0 , 0 , p_x^2*q_y^2 , ## 6
+								2*q_x^2*p_y*q_y , 0 , 2*p_x*q_x*p_y*q_y , 0 , 0 , 2*p_x*q_x*p_y*q_y , ## 12
+								0 , 2*p_x^2*p_y*q_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18
+								0 , p_x^2*p_y^2
+								) ,
+							c ( 0 , q_x^2*q_y^2 , p_x*q_x*q_y^2 , 0 , p_x*q_x*q_y^2 , p_x^2*q_y^2 , ## 6
+								q_x^2*q_y*p_y , 0 , p_x*q_x*p_y*q_y , q_x^2*q_y*p_y , 0 , 3*q_x*p_x*q_y*p_y , ## 12
+								0 , 2*p_x^2*q_y*p_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18
+								0 , p_x^2*p_y^2
+								)
+						)	
+	
+}
+
+
+
+#### netural portion for ld across site.
 MakeMatrices <- function ( states ) {
 	#recover()
 	x <- y <- numeric ( 8 )
@@ -11,8 +43,6 @@ MakeMatrices <- function ( states ) {
 	W <- W [ order ( W$V1 , W$V2 , decreasing = T ) , ]
 	return ( list ( S = S , W = W ) )
 }
-
-
 
 one.locus.states <- c ( combn ( seq ( 1,8,1) , 2 , simplify = F ) , list ( 1,2,5,6 ) )
 #one.locus.states <- split ( one.locus.states , rownames ( one.locus.states ) ); names ( one.locus.states ) <-NULL
@@ -39,7 +69,7 @@ while ( i < ( length ( state.matrices ) + 1 ) ) {
 	#head(state.matrices [ - c (1:i)])
 }
 GetNumerator <- function ( state.one , state.two , f, N , r_x , r_y ) {
-	#if ( recover.on ) recover()
+	#recover()
 	test.state <- rbind ( state.one$S [ 1 , ] , state.one$W )
 	test.state <- test.state [ order ( test.state$V1 , test.state$V2 , decreasing = T ) , ]
 	test.state <- test.state [ 1 : 4 , ]
@@ -196,12 +226,17 @@ GetDenominator <- function ( state.one , state.two , f, N , r_x , r_y ) {
 LDCalc <- function ( f , N , r_x , r_y , state.matrices ) {
 	
 	#recover()
+	
+	
+	
+	
+	## Neutral phase transition matrix
 	transition.mat <- matrix ( NA , nrow = 20 , ncol = 20 )
 	for ( i in 1 : 20 ) {
 		for ( j in 1 : 20 ) {
 			num <- GetNumerator (state.matrices [[ i ]] , state.matrices [[ j ]] , f , N , r_x , r_y )
 			denom <- GetDenominator (state.matrices [[ i ]] , state.matrices [[ j ]] , f , N , r_x , r_y )
-			transition.mat [ i , j ] <- num / denom
+			transition.mat [ i , j ] <- ifelse ( num == 0 & denom == 0 , 0 , num / denom )
 		}
 	}
 	
@@ -219,7 +254,7 @@ LDCalc <- function ( f , N , r_x , r_y , state.matrices ) {
 	tt_ijik_s <- sum ( phi_b * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
 	tt_ijkl_s <- sum ( phi_c * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
 	
-	( tt_ijij_s - 2 * tt_ijik_s + tt_ijkl_s ) / ( 1 + tt_ijkl_s )
+	( tt_ijij_s - 2 * tt_ijik_s + tt_ijkl_s ) / ( tt_ijkl_s )
 	
 }
 
@@ -230,19 +265,31 @@ r_x <- 10^-4
 r_y <- 10^-4
 LDCalc ( f = 0.05 , N = 100000 , r_x = 10^-4 , r_y = 10^-4 , state.matrices )
 
-my.freqs <- seq ( 0.01 , 0.1 , 0.01 )
+my.freqs <- c ( 0.001 , seq ( 0.01 , 0.1 , 0.01 ) )
 my.dist <- seq ( 0 , 10^-3 , length.out = 100 ) 
 my.ld <- list ()
-for ( j in 1 : 10 ) {
+for ( j in 1 : 11 ) {
 	my.ld [[ j ]] <- numeric ( 100 )
 	for ( i in 1 : 100 ) {
 		my.ld [[ j ]] [ i ] <-  LDCalc ( f = my.freqs [ j ] , N = 100000 , r_x = my.dist [ i ] , r_y = my.dist [ i ] , state.matrices )
 	}
 }
+
+
+tt_ijij_w <- ( 36 + 14 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
+tt_ijik_w <- ( 24 + 13 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
+tt_ijkl_w <- ( 22 + 13 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
+neutral.ld <- ( tt_ijij_w - 2 * tt_ijik_w + tt_ijkl_w ) / ( 1 + tt_ijkl_w - N^2)
+
+
+
+
+
 save ( my.ld , file = "LDvals.Robj")
 cols <- rainbow ( 10 )
 plot ( my.ld [[ 1 ]] , type = "l"  )
-matplot ( do.call ( cbind, my.ld ) , type = "l")
+matplot ( do.call ( cbind, my.ld ) , type = "l" , xlab = "4Nr" , xaxt = "n" , col = brewer.pal ( 11 , "Spectral" ) , lty = 1 , bty = "n" , ylab = "sigma^2_d" )
+axis ( 1 , at = c ( 0 , 49 , 99 ) , labels = c ( "0" , "100" , "200" ) )
 
 
 on <- function ( ) recover.on <<- T
