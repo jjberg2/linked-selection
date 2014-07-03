59cc0039c51ec1a80bd892d2df9ac4a5d8e925c2
Jeremy J Berg <jeremy.jackson.berg@gmail.com>
2014-07-03 10:59:43 -0700

    Clean up

--- Scripts/LDCalc.R
+++ Scripts/LDCalc.R
@@ -1,7 +1,9 @@
+setwd("/Users/JeremyBerg/Documents/Academics/StandingSweep/Scripts")
 library ( expm )
 library ( combinat )
 library ( plyr )
 library ( RColorBrewer )
+
 
 f <- 0.10
 N <- 100000
@@ -9,24 +11,37 @@ r_x <- 10^-4
 r_y <- 10^-4
 s <- 0.01
 #### selected portion for ld across site
-SelTransMat <- function ( r_x , r_y , s = f , N ) {
+SelTransMat <- function ( r_x , r_y , s , f , N ) {
 	p_x <- 1 - exp ( - r_x / s * log ( ( N - 1 ) / f - ( N - 1 ) ) )
 	q_x <- 1 - p_x
 	p_y <- 1 - exp ( - r_y / s * log ( ( N - 1 ) / f - ( N - 1 ) ) )
 	q_y <- 1 - p_y
 	
-	trans.mat <- matrix ( 	c ( 
-								q_x^2*q_y^2 , 0 , 2*p_x*q_x*q_y^2 , 0 , 0 , p_x^2*q_y^2 , ## 6
-								2*q_x^2*p_y*q_y , 0 , 2*p_x*q_x*p_y*q_y , 0 , 0 , 2*p_x*q_x*p_y*q_y , ## 12
-								0 , 2*p_x^2*p_y*q_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18
-								0 , p_x^2*p_y^2
-								) ,
-							c ( 0 , q_x^2*q_y^2 , p_x*q_x*q_y^2 , 0 , p_x*q_x*q_y^2 , p_x^2*q_y^2 , ## 6
-								q_x^2*q_y*p_y , 0 , p_x*q_x*p_y*q_y , q_x^2*q_y*p_y , 0 , 3*q_x*p_x*q_y*p_y , ## 12
-								0 , 2*p_x^2*q_y*p_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18
-								0 , p_x^2*p_y^2
-								)
-						)	
+	trans.mat <- matrix ( c (	
+								c ( 
+									q_x^2*q_y^2 , 0 , 2*p_x*q_x*q_y^2 , 0 , 0 , p_x^2*q_y^2 , ## 6
+									2*q_x^2*p_y*q_y , 0 , 2*p_x*q_x*p_y*q_y , 0 , 0 , 2*p_x*q_x*p_y*q_y , ## 12
+									0 , 2*p_x^2*p_y*q_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18
+									0 , p_x^2*p_y^2
+									) ,
+								c ( 
+									0 , q_x^2*q_y^2 , p_x*q_x*q_y^2 , 0 , p_x*q_x*q_y^2 , p_x^2*q_y^2 , ## 6
+									q_x^2*q_y*p_y , 0 , p_x*q_x*p_y*q_y , q_x^2*q_y*p_y , 0 , 3*q_x*p_x*q_y*p_y , ## 12
+									0 , 2*p_x^2*q_y*p_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18
+									0 , p_x^2*p_y^2
+									) , 
+								rep ( 0 , 20 ) ,
+								c ( 
+									0 , 0 , 0 , q_x^2*q_y^2 , 2*p_x*q_x*q_y^2 , p_x^2*q_y^2 , ## 6
+									0 , 0 , 0 , 2*q_x^2*p_y*q_y , 0 , 4*q_x*p_x*q_y*p_y , ## 12
+									0 , 2*p_x^2*p_y*q_y , q_x^2*p_y^2 , 0 , 2*p_x*q_x*p_y^2 , 0 , ## 18 
+									0 , p_x^2*p_y^2 
+									) ,
+								rep ( 0 , 20 * 16 ) 
+							) ,
+							nrow = 20 , ncol = 20 , byrow = T
+						)
+		return ( trans.mat )	
 	
 }
 
@@ -223,11 +238,12 @@ GetDenominator <- function ( state.one , state.two , f, N , r_x , r_y ) {
 		return ( rec_x + rec_y + coal_s + coal_w )
 	}
 }
-LDCalc <- function ( f , N , r_x , r_y , state.matrices ) {
+LDCalc <- function ( f , s , N , r_x , r_y , state.matrices ) {
 	
 	#recover()
 	
-	
+	## Selection phase transition matrix
+	sel.trans <- SelTransMat ( r_x , r_y , s , f , N )
 	
 	
 	## Neutral phase transition matrix
@@ -246,6 +262,20 @@ LDCalc <- function ( f , N , r_x , r_y , state.matrices ) {
 	tt_ijkl_w <- ( 22 + 13 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
 	
 	
+	# LD with sweep
+	phi_a <- ( c ( 1 , rep ( 0 , 19 ) ) %*% sel.trans %*% ( transition.mat %^%10 ) ) [ 18 : 20 ]
+	phi_b <- ( c ( 0 , 1 , rep ( 0 , 18 ) ) %*% sel.trans %*% ( transition.mat %^%10 ) ) [ 18 : 20 ]
+	phi_c <- ( c ( 0 , 0 , 0 , 1 , rep ( 0 , 16 ) ) %*% sel.trans %*% ( transition.mat %^%10 ) ) [ 18 : 20 ]
+	# reweighted selected values
+	tt_ijij_s <- sum ( phi_a * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
+	tt_ijik_s <- sum ( phi_b * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
+	tt_ijkl_s <- sum ( phi_c * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
+	
+	sweep.ld <- ( tt_ijij_s - 2 * tt_ijik_s + tt_ijkl_s ) / ( tt_ijkl_s )
+	
+	
+	
+	# ld without sweep portion
 	phi_a <- ( c ( 1 , rep ( 0 , 19 ) ) %*% ( transition.mat %^%10 ) ) [ 18 : 20 ]
 	phi_b <- ( c ( 0 , 1 , rep ( 0 , 18 ) ) %*% ( transition.mat %^%10 ) ) [ 18 : 20 ]
 	phi_c <- ( c ( 0 , 0 , 0 , 1 , rep ( 0 , 16 ) ) %*% ( transition.mat %^%10 ) ) [ 18 : 20 ]
@@ -254,48 +284,139 @@ LDCalc <- function ( f , N , r_x , r_y , state.matrices ) {
 	tt_ijik_s <- sum ( phi_b * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
 	tt_ijkl_s <- sum ( phi_c * c ( tt_ijij_w , tt_ijik_w , tt_ijkl_w ) )
 	
-	( tt_ijij_s - 2 * tt_ijik_s + tt_ijkl_s ) / ( tt_ijkl_s )
+	no.sweep.ld <- ( tt_ijij_s - 2 * tt_ijik_s + tt_ijkl_s ) / ( tt_ijkl_s )	
+	return ( no.sweep.ld )
 	
 }
 
 
-f <- 0.10
-N <- 100000
-r_x <- 10^-4
-r_y <- 10^-4
-LDCalc ( f = 0.05 , N = 100000 , r_x = 10^-4 , r_y = 10^-4 , state.matrices )
+# f <- 0.10
+# N <- 100000
+# r_x <- 10^-4
+# r_y <- 10^-4
+# LDCalc ( f = 0.05 , 0.01 , N = 100000 , r_x = 10^-4 , r_y = 10^-4 , state.matrices )
 
-my.freqs <- c ( 0.001 , seq ( 0.01 , 0.1 , 0.01 ) )
-my.dist <- seq ( 0 , 10^-3 , length.out = 100 ) 
+my.freqs <- c ( 0.001 , 0.002 , 0.005 , 0.008 , 0.01 , seq ( 0.02 , 0.1 , 0.02 ) )
+my.dist <- seq ( 10^-7 , 10^-3 , length.out = 20 ) 
 my.ld <- list ()
 for ( j in 1 : 11 ) {
-	my.ld [[ j ]] <- numeric ( 100 )
-	for ( i in 1 : 100 ) {
-		my.ld [[ j ]] [ i ] <-  LDCalc ( f = my.freqs [ j ] , N = 100000 , r_x = my.dist [ i ] , r_y = my.dist [ i ] , state.matrices )
+	my.ld [[ j ]] <- numeric ( 20 )
+	for ( i in 1 : 20 ) {
+		my.ld [[ j ]] [ i ] <-  LDCalc ( f = my.freqs [ j ] , s = 0.01 , N = 100000 , r_x = my.dist [ i ] , r_y = my.dist [ i ] , state.matrices )
 	}
+}
+save ( my.ld , file = "../Output/My.LD.calc.Robj")
+
+
+
+temp <- run.ms.f ( n.sam = 20 , f = 0.001 , s = 0.01 , no.sweep = T , reps = 100 , N = 10000 , n.loc = 40 )
+
+
+
+run.ms.f<-function( n.sam , f , s , no.sweep , reps , N , n.loc ){
+	recover()
+	source ( "/Users/JeremyBerg/Documents/Academics/StandingSweep/Scripts/SweepFromStandingSim.R")
+	setwd("/Users/JeremyBerg/Documents/Academics/StandingSweep")
+	freqs <- SweepFromStandingSim ( N , s , f , reps , no.sweep , cond.on.loss = T , cond.on.fix = T , display.rep.count = T ) [[ 1 ]]
+	
+	my.trajectories <- apply ( freqs , 1 , function ( x ) cbind ( 0 : ( length ( x [ x != 0 ] ) ) / ( 4*N ) , c ( rev ( x [ x != 0 ]  ) , 0 ) ) )
+	#recover()
+	ld.stats <- list ()
+	for ( i in 1 : length ( my.trajectories ) ) {
+		header.material <- c ( "1" , "1" , paste ( "n:" , nrow ( my.trajectories [[ i ]] ) ) )
+		write ( header.material , file = paste ( "Working/freq.traj." , i , sep = "" ) )
+		write.table ( my.trajectories [[  i ]] , file = paste ( "Working/freq.traj." , i , sep = "" ) , quote = F , row.names = F , col.names = F , append = T , sep = "\t" )
+		system(paste("Scripts/msseldir/mssel ",n.sam," 1 0 ",n.sam," Working/freq.traj.",i," 20 -t 10000.0 -r 2000.0 " , n.loc , " > Output/myseqdata" , i ,sep="") )
+	}
+	for ( i in 1 : length ( my.trajectories ) ) {
+		ld.stats [[ i ]] <- LDSimCalc ( paste ( "Output/myseqdata" , i , sep = "" ) )
+		ld.stats [[ i ]] [[ 4 ]] <- cut ( ld.stats [[ i ]] [[ 3 ]] , 0:n.loc/n.loc , include.lowest = T ) ; i = i+1 ; i 
+	}
+	sig.sq <- matrix ( NA , nrow = n.loc , ncol = n.loc )
+	r.sq <- matrix ( NA , nrow = n.loc , ncol = n.loc )
+	for ( i in levels ( ld.stats [[1]][[4]]) ) {
+		for ( j in levels ( ld.stats [[1]][[4]]) ) {
+			site.one <- lapply ( ld.stats , function ( x ) which ( x [[ 4 ]] == i ) )
+			site.two <- lapply ( ld.stats , function ( x ) which ( x [[ 4 ]] == j ) )
+			temp <- mapply ( function ( x , y , z ) { D.stat.sq <- x [[ 1 ]] [ y , z ] ; denom <- x [[ 2 ]] [ y , z ]; return ( list ( D.stat.sq , denom ) ) } , x = ld.stats , y = site.one , z = site.two )
+			sig.sq [ which ( levels ( ld.stats [[1]][[4]]) == i ) , which ( levels ( ld.stats [[1]][[4]]) == j ) ] <- mean ( unlist ( temp [ 1 , ] ) ) / mean ( unlist ( temp [ 2 , ] ) )
+			r.sq [ which ( levels ( ld.stats [[1]][[4]]) == i ) , which ( levels ( ld.stats [[1]][[4]]) == j ) ] <- mean ( unlist ( temp [ 1 , ] ) / unlist ( temp [ 2 , ] ) )	
+		}
+		print ( i )
+	}
+	return ( list ( sig.sq , r.sq ) )
+}
+
+LDSimCalc <- function ( file.name ) {
+	
+	#recover()
+	my.lines <- readLines ( file.name )
+	pos.line.id <- grep ( "pos" , my.lines )
+	pos.line <- my.lines [ pos.line.id ]
+	positions <- as.numeric ( strsplit ( pos.line , " ")[[ 1 ]] [ -1 ] )
+	geno.lines <- my.lines [ ( pos.line.id + 1 ) : length ( my.lines ) ]
+	split.geno.lines <-  lapply ( strsplit ( geno.lines , "" ) , as.numeric )
+	geno.mat <- do.call ( rbind , split.geno.lines )
+	D.stat.sq <- cov ( geno.mat )^2
+	freqs <- colMeans ( geno.mat )
+	rt.vars <- sqrt ( freqs * ( 1 - freqs ) )
+	var.prod.mat <- rt.vars %o% rt.vars
+	return ( list ( D.stat.sq , var.prod.mat , positions ) )
+	
 }
 
 
-tt_ijij_w <- ( 36 + 14 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
-tt_ijik_w <- ( 24 + 13 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
-tt_ijkl_w <- ( 22 + 13 * N * ( r_x + r_y ) + ( N * ( r_x + r_y ) )^2 ) / ( 18 + 13 * N * ( r_x + r_y ) )
-neutral.ld <- ( tt_ijij_w - 2 * tt_ijik_w + tt_ijkl_w ) / ( 1 + tt_ijkl_w - N^2)
+
+
+
+
+if ( FALSE ) {
+
+
+
+NeutralLD <- function ( r , N ) {
+
+	tt_ijij_w <- ( 36 + 14 * N * ( r ) + ( N * ( r ) )^2 ) / ( 18 + 13 * N * ( r ) )
+	tt_ijik_w <- ( 24 + 13 * N * ( r ) + ( N * ( r ) )^2 ) / ( 18 + 13 * N * ( r ) )
+	tt_ijkl_w <- ( 22 + 13 * N * ( r ) + ( N * ( r ) )^2 ) / ( 18 + 13 * N * ( r ) )
+	neutral.ld <- ( tt_ijij_w - 2 * tt_ijik_w + tt_ijkl_w ) / ( tt_ijkl_w )
+
+}
+
+lapply ( my.dist , function ( x ) NeutralLD ( 2*x , 10000 ) )
 
 
 
 
 
-save ( my.ld , file = "LDvals.Robj")
-cols <- rainbow ( 10 )
-plot ( my.ld [[ 1 ]] , type = "l"  )
-matplot ( do.call ( cbind, my.ld ) , type = "l" , xlab = "4Nr" , xaxt = "n" , col = brewer.pal ( 11 , "Spectral" ) , lty = 1 , bty = "n" , ylab = "sigma^2_d" )
-axis ( 1 , at = c ( 0 , 49 , 99 ) , labels = c ( "0" , "100" , "200" ) )
+
+
+
+
+
+#save ( my.ld , file = "LDvals.Robj")
+#cols <- rainbow ( 10 )
+#plot ( my.ld [[ 1 ]] , type = "l"  )
+par ( mfrow = c ( 1 , 2 ) )
+matplot ( do.call ( cbind, my.ld ) [ 1:10,] , type = "l" , xlab = "4Nr" , xaxt = "n" , col = brewer.pal ( 10 , "Spectral" ) , lty = 1 , bty = "n" , ylab = "sigma^2_d" , yaxt = "n" , ylim = c ( 0 , max ( unlist ( my.ld ) , na.rm = T ) ) )
+axis ( 1 , at = c ( 1 , 5 , 10 ) , labels = c ( "0" , "50" , "100" ) )
+axis ( 2 , at = c ( 0 , 0.15 , 0.3 , 0.45 ) )
+matplot ( do.call ( cbind, ld.w.sweep ) [ 1:10,] , type = "l" , xlab = "4Nr" , xaxt = "n" , col = brewer.pal ( 10 , "Spectral" ) , lty = 1 , bty = "n" , ylab = "sigma^2_d" )
+axis ( 1 , at = c ( 1 , 5 , 10 ) , labels = c ( "0" , "50" , "100" ) )
+legend ( "topright" , legend = my.freqs , col = brewer.pal ( 10 , "Spectral" ) , lty = 1 , bty = "n")
+
+
+
+
+
+
+
 
 
 on <- function ( ) recover.on <<- T
 off <- function ( ) recover.on <<- F
 
-
+}
 
 
 
