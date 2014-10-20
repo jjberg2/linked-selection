d75f4820541a534337aa6c84140e5fbe3ade1c5e
Jeremy Berg <jeremy.jackson.berg@gmail.com>
2014-10-20 13:20:47 -0700

    updated ms soft sweep file

--- Scripts/run_ms_soft_sweeps.R
+++ Scripts/run_ms_soft_sweeps.R
@@ -1,37 +1,52 @@
-source ("SweepFromStandingSim.R")
+source('~/Documents/Academics/CoopLab/Projects/StandingSweeps/Scripts/SweepFromStandingSim.R', chdir = TRUE)
 
-run.ms.f<-function ( f.index , n.sam = 2 , get.site.density = TRUE , recom = FALSE ) {
 
-	my.file <- paste ( "mssel_f" , n.sam , f.index , ".out" , sep = "" )
+real.fs <- c ( 0.001 , 0.005 , 0.01 , seq ( 0.02 , 0.2 , length = 10 ) )
+my.runs <- lapply ( real.fs , function ( x ) SweepFromStandingSim ( N = 10000 , s = 0.01 , f = x , reps = 1000 , no.sweep = FALSE , cond.on.loss = TRUE , cond.on.fix = TRUE , time.factor = 1 , display.rep.count = T ) )
+save ( my.runs , file = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/Sims/11000freq.trajectories.Rdata")
+
+
+
+run.ms.f <- function ( runs , n.sam = 2  , N , path , get.site.density = TRUE , recom = FALSE ) {
+	#recover()
+	my.file <- paste ( path , "Sims/mssel_f" , n.sam , f.index , ".out" , sep = "" )
 
 	system ( paste ( "rm " , my.file ) )
-	for ( run in 1:5 ) {
-		load ( paste ( "run_cond_lost_" , run , ".Robj" , sep = "" ) )
-		for ( i in 1:100 ) {
-			my.freqs <- my.runs [[ f.index ]]$trees[[ i ]]$freqs
-			my.times<-0 : length ( my.freqs )
-			my.freqs <- c ( my.freqs , 0 )
-			
-			my.times <- my.times / ( 4*10e3 )
-	
-	#		recover()
-			header.material <- c ( "1" , "1" , paste ( "n:" , length ( my.times ) ) )
-			write ( file = paste ( "my.standing" , f.index , ".traj" , sep = "" ) , header.material )
-			write.table ( file = paste ( "my.standing" , f.index , ".traj" , sep = "" ) , cbind ( my.times , my.freqs ) , append=TRUE , sep = "\t" , quot = FALSE , col.nam = FALSE , row.name = FALSE )
-			cat( i ," " )
-			if ( get.site.density ) { 
-				system ( paste ( "msseldir/mssel " , n.sam , " 20 0 " , n.sam , " my.standing" , f.index , ".traj 0 -t 200. -r 200. 20000 | grep pos | cut -f 2 -d : >> " , my.file , sep = "" ) )
-				}	else	{   ##setup for the mo. to do freq. spectrum
-				system ( paste ( "msseldir/mssel " , n.sam , " " , 20 , " 0 " , n.sam , " my.standing" , f.index , ".traj 0 -t 200. -r " , recom , " 2 > myseqdata" , sep = "" ) ) 
-				spec <- get.freq.spec ( n.sam , num.sims = 20 )
-				recover()
-			}
+	#for ( run in 1:5 ) {
+	#	load ( paste ( "run_cond_lost_" , run , ".Robj" , sep = "" ) )
+	for ( i in 1: nrow ( runs ) ) {
+		my.freqs <- runs [ i , runs [ i , ] > 0 ]
+		my.times <- 0 : length ( my.freqs )
+		my.freqs <- c ( my.freqs , 0 )
+		
+		my.times <- my.times / ( 4*N  )
+
+#		recover()
+		header.material <- c ( "1" , "1" , paste ( "n:" , length ( my.times ) ) )
+		write ( file = paste ( path , "Sims/my.standing" , f.index , ".traj" , sep = "" ) , header.material )
+		write.table ( file = paste ( path , "Sims/my.standing" , f.index , ".traj" , sep = "" ) , cbind ( my.times , my.freqs ) , append = TRUE , sep = "\t" , quot = FALSE , col.nam = FALSE , row.name = FALSE )
+		cat( i ," " )
+		if ( get.site.density ) { 
+			system ( paste ( path , "Scripts/msseldir/mssel " , n.sam , " 20 0 " , n.sam , " " , path , "Sims/my.standing" , f.index , ".traj 0 -t 200. -r 200. 20000 | grep pos | cut -f 2 -d : >> " , my.file , sep = "" ) )
+		}	else	{   ##setup for the mo. to do freq. spectrum
+			system ( paste ( "Sims/msseldir/mssel " , n.sam , " " , 20 , " 0 " , n.sam , " my.standing" , f.index , ".traj 0 -t 200. -r " , recom , " 2 > myseqdata" , sep = "" ) ) 
+			spec <- get.freq.spec ( n.sam , num.sims = 20 )
+			#recover()
 		}
 	}
 }
 
-real.fs <- c ( 0.001 , seq ( 0.01 , 0.1 , length = 10 ) )
-my.runs <- lapply ( real.fs , function ( x ) SweepFromStandingSim ( N = 10000 , s = 0.01 , f = x , reps = 100 , no.sweep = FALSE , cond.on.loss = TRUE , cond.on.fix = TRUE , time.factor = 1 , display.rep.count = T ) )
+
+run.ms.f ( runs = my.runs [[ 1 ]] [[ 1 ]] , n.sam = 2 , N = 10000 , path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/" )
+
+load ( file = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/Sims/11000freq.trajectories.Rdata" )
+
+for ( f.index in 1 : length ( real.fs ) ) {
+	run.ms.f ( runs = my.runs [[ f.index ]] [[ 1 ]] , n.sam = 20 , N = 10000 , path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/" )
+}
+
+
+path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/"
 
 
 get.mut.density<-function(file){
@@ -49,11 +64,11 @@ get.mut.density<-function(file){
 pdf(file="pi_density.pdf")
 	plot(c(0,200),c(0,1),type="n",xlab="4NR",ylab=expression(pi[R]/pi[0]),cex.lab=1.5)
 	pi.over.f<-list()
-	for ( f.index in 1:5 ) {
+	for ( f.index in 1 : length ( real.fs ) ) {
 	#run.ms.f(f.index)
-		mut.density <- get.mut.density ( file = paste ( "mssel_f" , f.index , ".out" , sep = "" ) )
+		mut.density <- get.mut.density ( file = paste ( path,  "Sims/mssel_f2" , f.index , ".out" , sep = "" ) )
 		pi.over.f [[ f.index ]] <- mut.density
-		lines ( mut.density$x , mut.density$y/(5*100*20) , col = f.index )
+		lines ( mut.density$x , mut.density$y/(1000*20) , col = f.index )
 	}
 	fs<-1:5/100
 	rho=seq(0,200,length=1000)
@@ -70,12 +85,6 @@ pdf(file="pi_density.pdf")
 	legend("bottomright",legend= paste("f=",fs),col=1:5,lty=1,lwd=2,cex=1.5)
 dev.off()
  
-
-
-
-for(f.index in 1:5){
- 	run.ms.f(f.index=f.index,n.sam=2)
-}
  
 
 N=1e4
