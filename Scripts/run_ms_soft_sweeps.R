378a3ef334bc3460607c121defcc766e49c9ff65
Jeremy J Berg <jeremy.jackson.berg@gmail.com>
2014-10-23 12:27:35 -0700

    try again

--- Scripts/run_ms_soft_sweeps.R
+++ Scripts/run_ms_soft_sweeps.R
@@ -21,46 +21,6 @@ path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/"  ##Jeremy's mach
 path = "~/Dropbox/Linked_selection_models/Soft_sweeps_coal/LinkedSelection/" #graham's work machine
 
 
-# run.ms.f <- function ( runs , n.sam = 2  ,f.index, N , path , get.site.density = TRUE , recom = FALSE ) {
-	# #recover()
-	# my.file <- paste ( path , "Sims/mssel_f" , n.sam , f.index , ".out" , sep = "" )
-	# num.sims<-20
-	# system ( paste ( "rm " , my.file ) )
-	# #for ( run in 1:5 ) {
-	# #	load ( paste ( "run_cond_lost_" , run , ".Robj" , sep = "" ) )
-# <<<<<<< HEAD
-	# for ( i in 1 : nrow ( runs ) ) {
-# =======
-	# if (! get.site.density ) my.specs<-matrix(NA,nrow=n.sam,ncol=num.sims*nrow ( runs ))
-	
-	# for ( i in 1: nrow ( runs ) ) {
-# >>>>>>> 365183d10ab90e2997d38e70e8c3dd7c649aa2eb
-		# my.freqs <- runs [ i , runs [ i , ] > 0 ]
-		# my.times <- 0 : length ( my.freqs )
-		# my.freqs <- c ( my.freqs , 0 )
-		
-		# my.times <- my.times / ( 4*N  )
-
-# #		recover()
-		# header.material <- c ( "1" , "1" , paste ( "n:" , length ( my.times ) ) )
-		# write ( file = paste ( path , "Sims/my.standing" , f.index , ".traj" , sep = "" ) , header.material )
-		# write.table ( file = paste ( path , "Sims/my.standing" , f.index , ".traj" , sep = "" ) , cbind ( my.times , my.freqs ) , append = TRUE , sep = "\t" , quot = FALSE , col.nam = FALSE , row.name = FALSE )
-		# cat( i ," " )
-		# if ( get.site.density ) { 
-			# system ( paste ( path , "Scripts/msseldir/mssel " , n.sam , " 20 0 " , n.sam , " " , path , "Sims/my.standing" , f.index , ".traj 0 -t 200. -r 200. 20000 | grep pos | cut -f 2 -d : >> " , my.file , sep = "" ) )
-		# }	else	{   ##setup for the mo. to do freq. spectrum
-			# system ( paste ( "Sims/msseldir/mssel " , n.sam , " " , 20 , " 0 " , n.sam , " my.standing" , f.index , ".traj 0 -t 200. -r " , recom , " 2 >",path, "Sims/myseqdata" , sep = "" ) ) 
-			
-			# spec <- get.freq.spec ( n.sam , num.sims = num.sims, path=path )
-			# my.specs[,(1+(counter-1)*num.sims):(counter*num.sims)]<-spec
-			# counter<-counter+1
-			
-			# #recover()
-		# }
-	# }
-	# if (! get.site.density ) return(my.specs)
-# }
-
 
 
 # example of running one set of frequencies
@@ -75,40 +35,7 @@ for ( f.index in 1 : length ( real.fs ) ) {
 
 
 
-get.mut.density<-function(file){
-	myres = scan(file)
-	##myres*theta, note range of smoothing
-	myres<-myres*200
-	
-	mydens = density(myres,from=3,to=190,bw=2.0,na.rm=T)
-	mydens$y<-mydens$y*length(myres)
-	return(mydens)
-}
 
-get.freq.spec<-function(n,num.sims, path){
-	a<-system(paste("grep segsites ", path,"myseqdata",sep=""),intern=TRUE)
-	seg.sites<-sapply(a,function(b){as.numeric(strsplit(b,":")[[1]][2])})
-	polymorph<- seg.sites>0
-	seq.lines<-c(0,cumsum(polymorph*n)[-length(polymorph)])	
-	freq.specs<-sapply(0:(num.sims-1),function(iter){		
-		if(!polymorph[1+iter]) {freq.spec<-rep(0,n);return(freq.spec)}
-		positions<-read.table(paste(path, "Sims/myseqdata",sep=""),skip=5+4*iter+seq.lines[iter+1],nrow=1)
-#		print(positions[1])
-#		if(length(positions)==1){freq.spec<-rep(0,n);return(freq.spec)}		
-		seqs.raw<-scan(paste(path, "Sims/myseqdata",sep=""),skip=6+4*iter+seq.lines[iter+1],nline=n,what=character(),quiet=TRUE)
-		seqs<-sapply(seqs.raw,function(seq){as.numeric(strsplit(seq,"")[[1]])})
-		colnames(seqs)<-NULL
-		seqs<-t(seqs)
-		these.pos<-positions[-1]>0.5   ###why the -1 here? oh because positions has label
-		if(sum(these.pos)==0){freq.spec<-rep(0,n);return(freq.spec)}
-		seqs<-seqs[,these.pos] ##throw out first 1/2 of seq.
-		if(sum(these.pos)==1){freq.spec<-(1:n==sum(seqs)); return(freq.spec)}
-		mut.freq<-colSums(seqs)
-		freq.spec<- sapply(1:n,function(i){sum(mut.freq==i)})
-		return(freq.spec)
-	})
-return(freq.specs)
-}
 
 my.freqs.specs<- run.ms.f ( runs = my.runs [[ 1 ]] [[ 1 ]] ,f.index=1, n.sam = 10 , N = 10000 , path = path,get.site.density = FALSE , recom = 100)
 
