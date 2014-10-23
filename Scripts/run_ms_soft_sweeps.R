db9b66b615cb10a20c04d7237ca9cbcb194f7ac6
cooplab <gcoop79@gmail.com>
2014-10-23 16:07:59 -0700

    any changes to ms run before merging with Jeremy's new version

--- Scripts/run_ms_soft_sweeps.R
+++ Scripts/run_ms_soft_sweeps.R
@@ -1,15 +1,15 @@
-source('~/Documents/Academics/CoopLab/Projects/StandingSweeps/Scripts/SweepFromStandingSim.R', chdir = TRUE)
+#source('~/Documents/Academics/CoopLab/Projects/StandingSweeps/Scripts/SweepFromStandingSim.R', chdir = TRUE)
 
 
 real.fs <- c ( 0.001 , 0.005 , 0.01 , seq ( 0.02 , 0.2 , length = 10 ) )
 #my.runs <- lapply ( real.fs , function ( x ) SweepFromStandingSim ( N = 10000 , s = 0.01 , f = x , reps = 1000 , no.sweep = FALSE , cond.on.loss = TRUE , cond.on.fix = TRUE , time.factor = 1 , display.rep.count = T ) )
 #save ( my.runs , file = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/Sims/11000freq.trajectories.Rdata")
 
-#load( file = "~/../Shared/11000freq.trajectories.Rdata")  #graham's work machine
-load ( file = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/Sims/11000freq.trajectories.Rdata" )    ##Jeremy's machine
+load( file = "~/../Shared/11000freq.trajectories.Rdata")  #graham's work machine
+#load ( file = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/Sims/11000freq.trajectories.Rdata" )    ##Jeremy's machine
 
 #Sys.info()["nodename"]
-path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/"  ##Jeremy's machine
+#path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/"  ##Jeremy's machine
 path = "~/Dropbox/Linked_selection_models/Soft_sweeps_coal/LinkedSelection/" #graham's work machine
 
 
@@ -20,7 +20,7 @@ run.ms.f <- function ( runs , n.sam = 2  ,f.index, N , path , get.site.density =
 	system ( paste ( "rm " , my.file ) )
 	#for ( run in 1:5 ) {
 	#	load ( paste ( "run_cond_lost_" , run , ".Robj" , sep = "" ) )
-	if (! get.site.density ) my.specs<-matrix(NA,nrow=n.sam,ncol=num.sims*nrow ( runs ))
+	if (! get.site.density ){ my.specs<-matrix(NA,nrow=n.sam,ncol=num.sims*nrow ( runs )); counter<-1}
 	
 	for ( i in 1: nrow ( runs ) ) {
 		my.freqs <- runs [ i , runs [ i , ] > 0 ]
@@ -37,7 +37,7 @@ run.ms.f <- function ( runs , n.sam = 2  ,f.index, N , path , get.site.density =
 		if ( get.site.density ) { 
 			system ( paste ( path , "Scripts/msseldir/mssel " , n.sam , " 20 0 " , n.sam , " " , path , "Sims/my.standing" , f.index , ".traj 0 -t 200. -r 200. 20000 | grep pos | cut -f 2 -d : >> " , my.file , sep = "" ) )
 		}	else	{   ##setup for the mo. to do freq. spectrum
-			system ( paste ( "Sims/msseldir/mssel " , n.sam , " " , 20 , " 0 " , n.sam , " my.standing" , f.index , ".traj 0 -t 200. -r " , recom , " 2 >",path, "Sims/myseqdata" , sep = "" ) ) 
+			system ( paste (path, "Scripts/msseldir/mssel " , n.sam , " " , 20 , " 0 " , n.sam  , " " , path , "Sims/my.standing"  , f.index , ".traj 0 -t 200. -r " , recom , " 2 > ",path, "Sims/myseqdata" , sep = "" ) ) 
 			
 			spec <- get.freq.spec ( n.sam , num.sims = num.sims, path=path )
 			my.specs[,(1+(counter-1)*num.sims):(counter*num.sims)]<-spec
@@ -50,12 +50,6 @@ run.ms.f <- function ( runs , n.sam = 2  ,f.index, N , path , get.site.density =
 }
 
 
-run.ms.f ( runs = my.runs [[ 1 ]] [[ 1 ]] ,f.index=1, n.sam = 2 , N = 10000 , path = path )
-
-
-for ( f.index in 1 : length ( real.fs ) ) {
-	run.ms.f ( runs = my.runs [[ f.index ]] [[ 1 ]] ,f.index=f.index ,n.sam = 20 , N = 10000 , path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/" )
-}
 
 
 
@@ -70,7 +64,7 @@ get.mut.density<-function(file){
 }
 
 get.freq.spec<-function(n,num.sims, path){
-	a<-system(paste("grep segsites ", path,"myseqdata",sep=""),intern=TRUE)
+	a<-system(paste("grep segsites ", path,"Sims/myseqdata",sep=""),intern=TRUE)
 	seg.sites<-sapply(a,function(b){as.numeric(strsplit(b,":")[[1]][2])})
 	polymorph<- seg.sites>0
 	seq.lines<-c(0,cumsum(polymorph*n)[-length(polymorph)])	
@@ -93,6 +87,33 @@ get.freq.spec<-function(n,num.sims, path){
 	})
 return(freq.specs)
 }
+
+
+##setup for BATCH put inside if FALSE after
+
+recoms<- seq(1,200,length=20)
+f.freq.spec<-list()
+for(f.index in 1:length(real.fs)){
+	print(f.index)
+	collect.freq.spec<-numeric()
+	for(recom in recoms){
+		cat("recom",recom,"\n")
+		my.freqs.specs<- run.ms.f ( runs = my.runs [[ f.index ]] [[ 1 ]] ,f.index=1, n.sam = 10 , N = 10000 , path = path,get.site.density = FALSE , recom = recom)
+		collect.freq.spec<-cbind(collect.freq.spec,rowMeans(my.freqs.specs))
+		save(file=paste(path,"Sims/Freq_spec.Robj",sep=""),collect.freq.spec,f.freq.spec)
+	}
+	f.freq.spec[[f.index]]<-collect.freq.spec
+	save(file=paste(path,"Sims/Freq_spec.Robj",sep=""),collect.freq.spec,f.freq.spec)
+}
+
+if(FALSE){
+run.ms.f ( runs = my.runs [[ 1 ]] [[ 1 ]] ,f.index=1, n.sam = 2 , N = 10000 , path = path )
+
+
+for ( f.index in 1 : length ( real.fs ) ) {
+	run.ms.f ( runs = my.runs [[ f.index ]] [[ 1 ]] ,f.index=f.index ,n.sam = 20 , N = 10000 , path = "~/Documents/Academics/CoopLab/Projects/StandingSweeps/" )
+}
+
 
 my.freqs.specs<- run.ms.f ( runs = my.runs [[ 1 ]] [[ 1 ]] ,f.index=1, n.sam = 10 , N = 10000 , path = path,get.site.density = FALSE , recom = 100)
 
@@ -148,4 +169,6 @@ for(f.index in 1:5){
  }
  legend("bottomright",legend= paste("f=",fs),col=1:5,lty=1,lwd=2,cex=1.5)
 dev.off()
+
+}
  
